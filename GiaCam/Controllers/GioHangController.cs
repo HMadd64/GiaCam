9b569a558ff8aaeb446a48ff0e1f706bcf97f672
doant <doant@ACERASPIRE7>
2022-06-02 22:04:14 +0700

    Add project files.

---
+++ GiaCam/Controllers/GioHangController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GiaCam.Models;
+
+namespace GiaCam.Controllers
+{
+    public class GioHangController : Controller
+    {
+        dbGiaCamDataContext data = new dbGiaCamDataContext();
+        public List<GioHang> LayGioHang()
+        {
+            List<GioHang> list = Session["GioHang"] as List<GioHang>;
+            if (list == null)
+            {
+                list = new List<GioHang>();
+                Session["GioHang"] = list;
+            }
+            return list;
+        }
+        public ActionResult ThemGioHang(int maSP, string url)
+        {
+            List<GioHang> list = LayGioHang();
+            GioHang sp = list.Find(n => n.iMaSP == maSP);
+            if(sp == null)
+            {
+                sp = new GioHang(maSP);
+                list.Add(sp);
+                return Redirect(url);
+            }
+            else
+            {
+                sp.iSoLuong++;
+                return Redirect(url);
+            }
+        }
+        public ActionResult XoaGioHang(int maSP)
+        {
+            List<GioHang> list = LayGioHang();
+            GioHang sp = list.SingleOrDefault(n => n.iMaSP == maSP);
+            if(sp != null)
+            {
+                list.RemoveAll(n => n.iMaSP == maSP);
+                return RedirectToAction("GioHang");
+            }
+            if(list.Count == 0)
+            {
+                return RedirectToAction("Index", "SanPham");
+            }
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult CapNhatGioHang(int maSP, FormCollection f)
+        {
+            List<GioHang> list = LayGioHang();
+            GioHang sp = list.SingleOrDefault(n => n.iMaSP == maSP);
+            if(sp.iSoLuong != null)
+            {
+                sp.iSoLuong = int.Parse(f["txtSoLuong"].ToString());    
+            }
+            return RedirectToAction("GioHang");
+        }
+        public ActionResult XoaTatCaGioHang()
+        {
+            List<GioHang> list = LayGioHang();
+            list.Clear();
+            return RedirectToAction("Index", "SanPham");
+        }
+        private int TongSoLuong()
+        {
+            int iTongSL = 0;
+            List<GioHang> list = Session["GioHang"] as List<GioHang>;
+            if(list != null)
+            {
+                iTongSL = list.Sum(n => n.iSoLuong);
+            }
+            return iTongSL;
+        }
+        private double TongTien()
+        {
+            double dTongTien = 0;
+            List<GioHang> list = Session["GioHang"] as List<GioHang>;
+            if (list != null)
+            {
+                dTongTien = list.Sum(n => n.dThanhTien);
+            }
+            return dTongTien;
+        }
+        public ActionResult GioHang()
+        {
+            List<GioHang> list = LayGioHang();
+            if(list.Count == 0)
+            {
+                return RedirectToAction("Index", "SanPham");
+            }
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return View(list);
+        }
+        public ActionResult GioHangPartial()
+        {
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return PartialView();
+        }
+        // GET: GioHang
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GiaCam/Models/GioHang.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GiaCam.Models;
+
+namespace GiaCam.Models
+{
+    public class GioHang
+    {
+        dbGiaCamDataContext data = new dbGiaCamDataContext();
+        public int iMaSP { get; set; }
+        public string sTenSP { get; set; }
+        public string sAnhSP { get; set; }
+        public double dDonGia { get; set; }
+        public int iSoLuong { get; set; }   
+        public double dThanhTien
+        {
+            get { return dDonGia * iSoLuong; }
+        }
+
+        public GioHang(int maSP)
+        {
+            iMaSP = maSP;
+            SanPham sp = data.SanPhams.Single(n => n.MaSP == iMaSP);
+            sTenSP = sp.TenSP;
+            sAnhSP = sp.AnhSP;
+            dDonGia = double.Parse(sp.GiaSP.ToString());
+            iSoLuong = 1;
+        }
+    }
+}
