384aacf85c8c2b56bb8853a2b00088bd9dc25341
Trung Hiếu <106734395+HMadd64@users.noreply.github.com>
2022-06-23 23:30:37 +0700

    dangnhap

--- GiaCam/Controllers/AdminController.cs
+++ GiaCam/Controllers/AdminController.cs
@@ -49,6 +49,41 @@ namespace GiaCam.Controllers
             return RedirectToAction("NhaCungCap");
         }
         [HttpGet]
+        public ActionResult EditNCC(int id)
+        {
+            NhaCungCap ncc = db.NhaCungCaps.SingleOrDefault(n => n.MaNCC == id);
+            if (ncc == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(ncc);
+        }
+        [HttpPost]
+        public ActionResult EditNCC(int id, FormCollection collection)
+        {
+            var tenncc = collection["TenNCC"];
+            var diachi = collection["DiaChi"];
+            var sdt = collection["SDT"];
+            var ncc = from cc in db.NhaCungCaps where cc.MaNCC == id select cc;
+            if (ncc != null)
+            {
+                foreach (var item in ncc)
+                {
+                    if (item.MaNCC == id)
+                    {
+                        item.TenNCC = tenncc;
+                        item.DiaChi = diachi;
+                        item.SDT = sdt;
+                        UpdateModel(ncc);
+                        db.SubmitChanges();
+                        return RedirectToAction("NhaCungCap");
+                    }
+                }
+            }
+            return RedirectToAction("NhaCungCap");
+        }
+        [HttpGet]
         public ActionResult DeleteNCC(int id)
         {
             NhaCungCap ncc = db.NhaCungCaps.SingleOrDefault(n => n.MaNCC== id);
@@ -105,6 +140,37 @@ namespace GiaCam.Controllers
             return RedirectToAction("LoaiSanPham");
         }
         [HttpGet]
+        public ActionResult EditLoai(int id)
+        {
+            LoaiSanPham loai = db.LoaiSanPhams.SingleOrDefault(n => n.MaLoai == id);
+            if (loai == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(loai);
+        }
+        [HttpPost]
+        public ActionResult EditLoai(int id, FormCollection collection)
+        {
+            var tenloai = collection["TenLoai"];
+            var loai = from l in db.LoaiSanPhams where l.MaLoai == id select l;
+            if (tenloai != null)
+            {
+                foreach(var item in loai)
+                {
+                    if(item.MaLoai == id)
+                    {
+                        item.TenLoai = tenloai;
+                        UpdateModel(loai);
+                        db.SubmitChanges();
+                        return RedirectToAction("LoaiSanPham");
+                    }
+                }   
+            }
+            return RedirectToAction("LoaiSanPham");
+        }
+        [HttpGet]
         public ActionResult DeleteLoai(int id)
         {
             LoaiSanPham loai = db.LoaiSanPhams.SingleOrDefault(n => n.MaLoai == id);
@@ -143,10 +209,15 @@ namespace GiaCam.Controllers
 
         public ActionResult SanPham(int? page)
         {
-            int pageNum = (page ?? 1);
-            int pageSize = 5;
+            var onl = Session["TaiKhoan"];
+            if (onl != null)
+            {
+                int pageNum = (page ?? 1);
+                int pageSize = 5;
 
-            return View(db.SanPhams.ToList().OrderBy(n => n.MaSP).ToPagedList(pageNum, pageSize));
+                return View(db.SanPhams.ToList().OrderBy(n => n.MaSP).ToPagedList(pageNum, pageSize));
+            }
+            return RedirectToAction("DangNhap", "TaiKhoan");
         }
         [HttpGet]
         public ActionResult Create()
@@ -159,8 +230,8 @@ namespace GiaCam.Controllers
         [ValidateInput(false)]
         public ActionResult Create(SanPham sp, HttpPostedFileBase file)
         {
-            ViewBag.MaLoai = new SelectList(db.LoaiSanPhams.ToList().OrderBy(n => n.TenLoai), "Maloai", "TenLoai", sp.MaLoai);
-            ViewBag.MaNCC = new SelectList(db.NhaCungCaps.ToList().OrderBy(n => n.TenNCC), "MaNCC", "TenNCC", sp.MaNCC);
+            ViewBag.MaLoai = new SelectList(db.LoaiSanPhams.ToList().OrderBy(n => n.TenLoai), "Maloai", "TenLoai");
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps.ToList().OrderBy(n => n.TenNCC), "MaNCC", "TenNCC");
             if (file == null)
             {
                 ViewBag.ThongBao = "Vui lòng chọn ảnh sản phẩm!";
@@ -230,21 +301,76 @@ namespace GiaCam.Controllers
         [HttpGet]
         public ActionResult Edit(int id)
         {
-            ViewBag.MaLoai = new SelectList(db.LoaiSanPhams.ToList().OrderBy(n => n.TenLoai), "Maloai", "TenLoai");
-            ViewBag.MaNCC = new SelectList(db.NhaCungCaps.ToList().OrderBy(n => n.TenNCC), "MaNCC", "TenNCC");
             SanPham sp = db.SanPhams.SingleOrDefault(n => n.MaSP == id);
-            ViewBag.MaSP = sp.MaSP;
             if (sp == null)
             {
                 Response.StatusCode = 404;
                 return null;
             }
+            ViewBag.MaLoai = new SelectList(db.LoaiSanPhams.ToList().OrderBy(n => n.TenLoai), "Maloai", "TenLoai", sp.MaLoai);
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps.ToList().OrderBy(n => n.TenNCC), "MaNCC", "TenNCC", sp.MaNCC);
             return View(sp);
         }
         [HttpPost]
-        public ActionResult Edit(SanPham sp)
+        public ActionResult Edit(int id, HttpPostedFileBase fileUpdate, FormCollection collection)
         {
-            return RedirectToAction("SanPham");         
+            var tensp = collection["TenSP"];
+            var mota = collection["MoTa"];
+            var giasp = float.Parse(collection["GiaSP"].ToString());
+            var sl = int.Parse(collection["TonKho"]);
+            var ngaynhap = Convert.ToDateTime(collection["NgayNhap"]);
+            var hansd = Convert.ToDateTime(collection["HanSuDung"]);
+            var maloai = int.Parse(collection["MaLoai"]);
+            var mancc = int.Parse(collection["MaNCC"]);
+
+            var sanpham = from sp in db.SanPhams where sp.MaSP == id select sp;
+            if (tensp != null)
+            {
+                foreach (var item in sanpham)
+                {
+                    if (item.MaSP == id)
+                    {
+                        item.TenSP = tensp;
+                        item.MoTa = mota;
+
+                        ViewBag.MaLoai = new SelectList(db.LoaiSanPhams.ToList().OrderBy(n => n.TenLoai), "Maloai", "TenLoai", item.MaLoai);
+                        ViewBag.MaNCC = new SelectList(db.NhaCungCaps.ToList().OrderBy(n => n.TenNCC), "MaNCC", "TenNCC", item.MaNCC);
+                        if (fileUpdate == null)
+                        {
+                            ViewBag.ThongBao = "Vui lòng chọn ảnh sản phẩm!";
+                            return View();
+                        }
+                        else
+                        {
+
+                            var fileName = Path.GetFileName(fileUpdate.FileName);
+                            var path = Path.Combine(Server.MapPath("~/Content/Hinh_web"), fileName);
+                            if (System.IO.File.Exists(path))
+                            {
+                                ViewBag.ThongBao = "Hình ảnh đã tồn tại!";
+                            }
+                            else
+                            {
+                                fileUpdate.SaveAs(path);
+                            }
+                            item.AnhSP = fileName;
+                        }
+                        item.GiaSP = giasp;
+                        item.TonKho = sl;
+                        item.NgayNhap = ngaynhap;
+                        item.HanSuDung = hansd;
+                        item.MaLoai = maloai;
+                        item.MaNCC = mancc;
+
+                        UpdateModel(sanpham);
+                        db.SubmitChanges();
+                        return RedirectToAction("SanPham");
+                    }
+                }         
+            }
+            
+            return RedirectToAction("SanPham");
         }
     }
 }
+    
